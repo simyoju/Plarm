e952c095a5faf986c5641f32ed4e5e2f582f60ca
simyo <hj.sim@muzlive.com>
2023-09-08 11:54:23 +0900

    docs: moved info.plist and linked MainViewController

--- Plarm/Presentation/MainViewController.swift
+++ Plarm/Presentation/MainViewController.swift
@@ -5,4 +5,11 @@
 //  Created by Muzlive_Player on 2023/09/08.
 //
 
-import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
